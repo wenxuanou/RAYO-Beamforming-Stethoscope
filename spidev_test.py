61cc8e2ad28c8ff16b9b25f933cf81ce1c7aaf06
owen <owen@169-231-88-249.wireless.ucsb.edu>
2020-02-20 14:54:02 -0800

    added data saving and elapsed time

--- spidev_test.py
+++ spidev_test.py
@@ -7,16 +7,16 @@
 # Chip select: Pin 26 [CS1]
 
 import spidev
+import scipy.io as sio
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-    
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 spi = spidev.SpiDev()
 spi.open(0, 0) # bus number, device number
-spi.max_speed_hz = 7800000 # up to 125000000Hz = 125.0 MHz
+spi.max_speed_hz = 7800000 # SPI clock rate, up to 125000000Hz = 125.0 MHz
 spi.mode = 0b10 # clock polarity and phase (0b00 - 0b11)
 spi.no_cs = True 
 
@@ -24,7 +24,14 @@ spi.no_cs = True
 # Using dynamic sampling mode, erased empty channel
 Vref = 2.5
 chanNum = 2 
-volts = []
+volts = []  # Data read in buffer
+
+data = []   # Data to stored in matlab
+dataNum = 2048  # Number of Data to read in
+dataCount = 0   # Number of Data saved
+
+# Timing
+t = 0
 
 def inv_twos_comp(val, bits):
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
@@ -33,39 +40,53 @@ def inv_twos_comp(val, bits):
     return val
 
 def readADC(self):
-    #results = spi.readbytes(3)
-    #value = (results[0] << 16) + (results[1] << 8) + (results[2])
-    #hex_vals = (value & 0xFFFFFF)
-    #print((inv_twos_comp(value,24)))
-    #print(hex(hex_vals))
+    global dataCount, t
 
-    #Vref = 2.5
-    #volts = (value/(2**23-1))*(2 * Vref)
-    
-    #print(volts)
-    
-    ########################################
-    #For 2 channel sampling, chanNum = 2
-    for count in range(chanNum):
-        results = spi.readbytes(3)
-        value = (results[0] << 16) + (results[1] << 8) + (results[2])
-        value = inv_twos_comp(value, 24) 
-        volts[count] = (value/(2**23-1))*(2 * Vref)
-        
-    tstamp = (time.strftime("%Y %d %b %H.%M.%S"))# Create time stamp
-    print(tstamp, "Channel A: ", volts[0], "Channel B: ", volts[1]);
+    if dataCount < dataNum:
+        if dataCount == 0:
+            t = time.time()
+
+        #results = spi.readbytes(3)
+        #value = (results[0] << 16) + (results[1] << 8) + (results[2])
+        #hex_vals = (value & 0xFFFFFF)
+        #print((inv_twos_comp(value,24)))
+        #print(hex(hex_vals))
+
+        #Vref = 2.5
+        #volts = (value/(2**23-1))*(2 * Vref)
+
+        #print(volts)
+
+        ########################################
+        #For 2 channel sampling, chanNum = 2
+        for count in range(chanNum):
+            results = spi.readbytes(3)
+            value = (results[0] << 16) + (results[1] << 8) + (results[2])
+            value = inv_twos_comp(value, 24)
+            volts[count] = (value/(2**23-1))*(2 * Vref)
+
+        ## Display data
+        #tstamp = (time.strftime("%Y %d %b %H.%M.%S"))# Create time stamp
+        #print(tstamp, "Channel A: ", volts[0], "Channel B: ", volts[1])
+
+        # Save data
+        data.insert(dataCount, volts)
+
+        dataCount += 1
+    else:
+        elapsed = time.time() - t
+        sio.savemat("data.mat", {"data": data, "elapsed time": elapsed})
 
 # Setup interrupt listening data r
 GPIO.add_event_detect(17, GPIO.FALLING, callback = readADC, bouncetime = 300)
 
+
+
 if __name__ == "__main__":
     print("run")
-    
+
     for count in range(chanNum):
         volts.append(0)
-
-
-
 
 
 
